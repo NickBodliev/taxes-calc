fe1019e9f23ebf395cd349059b04520f50341481
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-10-31 14:32:11 +0100

    [dev] redirect from dashboard if no activity type is selected

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { auth } from '../firebase/initFirebase'
+import { db, auth } from '../firebase/initFirebase'
 import { onAuthStateChanged } from '@firebase/auth'
 import { Layout, Page } from '@shopify/polaris'
 import TaxesForm from '../components/TaxesForm'
 import History from '../components/History'
 import { useRouter } from "next/router";
+import { doc, getDoc } from "firebase/firestore";
 
 
 export default function Home() {
   const [user, setUser] = useState(null);
+  const [activityType, setActivityType] = useState(null);
   const router = useRouter();
   const redirect = (path) => { router.push(path) };
 
@@ -16,13 +18,21 @@ export default function Home() {
     if (user) {
         // User is signed in
         setUser(user);
+        getActivityType(user.email);
+        if(!activityType){
+          redirect('/auth');
+        }
     } else {
         // User is signed out
-        //setUser(null);
         redirect('/auth');
-        //useRouter().push('/auth');
     }
   });
+
+  const getActivityType = async (userEmail) => {
+    const docSnap = await getDoc(doc(db, "messages", userEmail));
+    const dbActivityType = await docSnap.data().activityType;
+    setActivityType(dbActivityType);
+  }
 
   return (
     <Page title="Details page">

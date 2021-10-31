4d72a678cbe8af52d826efb795ba45411f281c19
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-10-31 13:39:12 +0100

    [dev] account page front-end

--- components/auth/FirebaseAuth.js
+++ components/auth/FirebaseAuth.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { signInWithPopup, signOut, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth"
 import { auth } from '../../firebase/initFirebase'
 import { useRouter } from 'next/router';
-import { Card, EmptyState, Layout, Page } from '@shopify/polaris';
+import { Card, ContextualSaveBar, EmptyState, Layout, Page, Select } from '@shopify/polaris';
 
 function FirebaseAuth(props, { globalUser }) {
     const [user, setUser] = useState(globalUser);
@@ -56,14 +56,56 @@ function FirebaseAuth(props, { globalUser }) {
           });
     }
 
+    const [activityType, setActivityType] = useState('');
+
+  const handleSelectChange = value => {setActivityType(value); setUnsavedChanges(true);};
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+
+  const activityTypes = [
+    {label: 'Società di Capitali', value: 'societa-di-capitali'},
+    {label: 'Società di Persone', value: 'societa-di-persone'},
+    {label: 'Partita IVA Ordinaria', value: 'partita-iva-ordinaria'},
+    {label: 'Partita IVA Forfettaria', value: 'partita-iva-forfettaria'}
+  ];
+
     return (
       <Page>
+        { unsavedChanges == true &&
+          <ContextualSaveBar
+            message="Unsaved changes"
+            saveAction={{
+              onAction: () => console.log('add form submit logic'),
+              loading: false,
+              disabled: false,
+            }}
+            discardAction={{
+              onAction: () => console.log('add clear form logic'),
+            }}
+          />
+        }
         <Layout>
+          {user ?
+            <Layout.Section>
+              <Card
+                title="Select your activity type in order to start using Tax Tracker"
+                sectioned
+              >
+                <Select
+                  label="Activity Type"
+                  options={activityTypes}
+                  onChange={handleSelectChange}
+                  value={activityType}
+                  placeholder=" ~ Activity Type ~ "
+                  //error={ }
+                />
+              </Card>
+            </Layout.Section>
+          :
           <Layout.Section>
             <Card sectioned>
               <EmptyState
                 heading="Manage your taxes in a smart way"
-                action={(user ? {content: 'Log out', onAction: logout, destructive: true} : {content: 'Log in', onAction: login})}
+                action={{content: 'Log in', onAction: login}}
                 secondaryAction={{content: 'Learn more', url: 'http://simp.ly/p/fF4mxM'}}
                 image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
               >
@@ -71,6 +113,7 @@ function FirebaseAuth(props, { globalUser }) {
               </EmptyState>
           </Card>
           </Layout.Section>
+          }
         </Layout>
       </Page>
     )

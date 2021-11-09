722533181e29186d383df9717b4d19cb5a948b14
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-11-09 23:19:06 +0100

    [dev] Index table with button

--- components/PastRecords.js
+++ components/PastRecords.js
@@ -1,16 +1,31 @@
-import { Card, DataTable } from '@shopify/polaris';
+import { Button, Card, IndexTable, TextStyle } from '@shopify/polaris';
 import React, { useEffect, useState } from 'react'
+import {
+  DeleteMinor
+} from '@shopify/polaris-icons';
 
 function Messages({messages}) {
-    const [records, setRecords] = useState([])
+    const [records, setRecords] = useState([]);
+    const resourceName = {
+      singular: 'Record',
+      plural: 'Records',
+    };
 
     useEffect(() => {
         if(messages != null && messages != undefined){
-          let rows = messages.map(
-            ({year, earnings, taxes, guadagnoPuro}) =>
-              [year, earnings, taxes, guadagnoPuro]
-            );
-            console.log(rows);
+          const rows = messages.map(
+            ({year, earnings, taxes, guadagnoPuro}, index) => (
+                <IndexTable.Row id={year} key={year} position={index}>
+                <IndexTable.Cell>
+                  <TextStyle variation="strong">{year}</TextStyle>
+                </IndexTable.Cell>
+                <IndexTable.Cell>{earnings}</IndexTable.Cell>
+                <IndexTable.Cell>{taxes}</IndexTable.Cell>
+                <IndexTable.Cell>{guadagnoPuro}</IndexTable.Cell>
+                <Button onClick={() => alert(year)} icon={DeleteMinor}/>
+              </IndexTable.Row>
+            ),
+          );
             setRecords(rows);
         }
     }, [messages])
@@ -22,23 +37,21 @@ function Messages({messages}) {
     }else{
         return(
             <Card title="Past Records" sectioned>
-          <DataTable
-            columnContentTypes={[
-              'text',
-              'number',
-              'number',
-              'number',
-            ]}
-            headings={[
-              'Year',
-              'Earnings',
-              'Taxes',
-              'Net Earnings',
-            ]}
-            rows={records}
-          />
-        </Card>
-        )
+              <IndexTable
+                resourceName={resourceName}
+                itemCount={messages.length}
+                headings={[
+                  {title: 'Year'},
+                  {title: 'Earnings'},
+                  {title: 'Taxes'},
+                  {title: 'Guadagno Puro', hidden: false},
+                ]}
+                selectable={false}
+              >
+                {records}
+              </IndexTable>
+            </Card>
+      )
     }
 }
 
